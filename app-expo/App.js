17dd2bc73c9bf41f4a45c8e61f2d9e5ce5b63f4a
gabriel <gabrielfreitas1510@gmail.com>
2026-05-20 22:07:19 -0300

    Initial commit

---
+++ app-expo/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import HomePage from './src/pages/homePage'
+
+const App = () => {
+  return (
+    <HomePage/>  
+  );
+}
+
+export default App;
---
+++ app-expo/src/pages/homePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const HomePage = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text> Duda linda </Text>
+    </View>    
+  );
+}
+
+export default HomePage;
